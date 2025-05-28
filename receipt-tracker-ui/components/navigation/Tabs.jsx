ac3fb67ec96a4e72bc0d9397e4586e47e4cedf3c
chughes25 <connorhu12@gmail.com>
2025-05-28 00:34:30 -0500

    added navigation

---
+++ receipt-tracker-ui/components/navigation/Tabs.jsx
@@ -0,0 +1,16 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Summary from "../screens/Summary"
+import Analysis from "../screens/Analysis"
+import Capture from "../screens/Capture"
+
+const Tab = createBottomTabNavigator();
+
+function Tabs() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Summary" component={Summary} />
+            <Tab.Screen name="Analysis" component={Analysis} />
+            <Tab.Screen name="Capture" component={Capture} />
+        </Tab.Navigator>
+    )
+}
