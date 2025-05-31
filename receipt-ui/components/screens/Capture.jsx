fbef2f112c763ce50d0ececf7f2f904a78b831d6
chughes25 <145420072+chughes25@users.noreply.github.com>
2025-05-30 20:27:07 -0500

    started capture screen

--- receipt-ui/components/screens/Capture.jsx
+++ receipt-ui/components/screens/Capture.jsx
@@ -2,6 +2,14 @@ import * as ImagePicker from 'expo-image-picker'
 import { useState } from 'react'
 import { Button, Image, View, StyleSheet } from 'react-native'
 
-function PickImage() {
-    const result = await ImagePicker.launchImageLibraryAsync
+export default function UploadImage() {
+    const PickImage = async () => {
+        const result = await ImagePicker.launchImageLibraryAsync({
+            allowsEditing: true,
+            aspect: [4,3],
+            quality: 0.7
+        })
+
+        
+    }
 }
