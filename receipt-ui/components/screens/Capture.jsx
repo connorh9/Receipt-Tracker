8a351eae35a8c0b9d846067f5a0095026fbfb097
chughes25 <145420072+chughes25@users.noreply.github.com>
2025-06-28 20:27:18 -0500

    updated camera permissions message

--- receipt-ui/components/screens/Capture.jsx
+++ receipt-ui/components/screens/Capture.jsx
@@ -32,6 +32,7 @@ export default function UploadImage() {
         }
 
         const result = await ImagePicker.launchCameraAsync({
+            allowsEditing: true,
             
         })
     }
